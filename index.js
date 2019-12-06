f0609140769ddc89c40a00b6229d4c129e79a6f5
github username <github email address>
2019-12-05 22:03:51 -0800

    accidentally added other files, so removed those, and also updated the CSS

--- index.js
+++ index.js
@@ -25,7 +25,9 @@ class HomePage{
         this.renderMid(rootMid);
         bodyElement.appendChild(rootMid);
         this.renderBottom(rootBottom);
-        bodyElement.appendChild(rootBottom)
+        bodyElement.appendChild(rootBottom);
+
+
     }
 
     static renderTop(rootTop){
@@ -115,7 +117,9 @@ class HomePage{
             .then(resp => resp.json())
             .then(json => {
                 localStorage.setItem("userId", json["id"]);
-                localStorage.setItem("userWins", json["wins"])
+                localStorage.setItem("userWins", json["wins"]);
+                playBackground();
+                buttonClickAudio.play();
                 CharacterSelection.renderPage(bodyElement);
             })
             .catch()
@@ -127,7 +131,9 @@ class HomePage{
             .then(resp => resp.json())
             .then(json => {
                 localStorage.setItem("userId", json["id"]);
-                localStorage.setItem("userWins", json["wins"])
+                localStorage.setItem("userWins", json["wins"]);
+                playBackground();
+                buttonClickAudio.play();
                 CharacterSelection.renderPage(bodyElement);
             })
         })
@@ -144,9 +150,11 @@ class HomePage{
         rootBottom.appendChild(howPlayButton)
 
         aboutButton.addEventListener('click', function(){
+            buttonClickAudio.play()
             About.renderPage(bodyElement);
         })
         howPlayButton.addEventListener('click', function(){
+            buttonClickAudio.play()
             HowToPlay.renderPage(bodyElement);
         })
     }
@@ -175,6 +183,7 @@ class About{
         backButton.textContent = 'Back';
         aboutTop.appendChild(backButton);
         backButton.addEventListener('click', function(){
+            buttonClickAudio.play()
             HomePage.renderPage(bodyElement);
         })
     }
@@ -209,6 +218,7 @@ class HowToPlay{
         backButton.textContent = 'Back';
         howTop.appendChild(backButton);
         backButton.addEventListener('click', function(){
+            buttonClickAudio.play();
             HomePage.renderPage(bodyElement);
         })
     }
@@ -259,25 +269,23 @@ class CharacterSelection{
         fetch(`http://localhost:3000/characters`)
             .then(resp => resp.json())
             .then(json => {
-                console.log(json)
                 for (let i = 0; i < json.length; i++)
                 {
                     let icon = document.createElement("img"); //input element, text
                     icon.setAttribute('class',`fighter-icon`);
                     icon.setAttribute('id',json[i].id);
-                    // console.log(json[i].wins_required, parseInt(localStorage.getItem("userWins")) )
                     if(parseInt(localStorage.getItem("userWins")) >= json[i].wins_required)
                     {
                         console.log("it hits", json[i].win_required)
                         icon.src = json[i].icon_img
                         icon.addEventListener('click', function(e){
-                            localStorage.setItem("fighterId", e.target.id)
-                            dynamicDescription.textContent = json[e.target.id].description;
+                            localStorage.setItem("fighterId", json[i].id)
+                            dynamicDescription.textContent = json[i].description;
                         });
                     }
                     else
                     {
-                        icon.src = 'https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500'
+                        icon.src = 'https://i.imgur.com/VNYNDVn.png'
                     }
                     iconContainer.appendChild(icon);
                 }
@@ -293,13 +301,12 @@ class CharacterSelection{
         chooseBottom.appendChild(continueButton);
 
         continueButton.addEventListener('click', function(e){
+            buttonClickAudio.play();
             ChooseBackground.renderPage(bodyElement);
-            showDivs(slideIndex);
         })
     }
 }
 /*----------------------------------------------------------------------------------------*/
-
 /*--------------------- Renders the Background page ------------------------------------- */
 class ChooseBackground{
     static renderPage(bodyElement){
@@ -324,42 +331,41 @@ class ChooseBackground{
         backgroundTop.appendChild(titleText);
     }
 
+
     static renderMid(backgroundMid){
         let backgroundContainer = document.createElement('div');
         backgroundContainer.id = "backgrounds"
-        for (let i = 1; i <= 8; i++)
-        {
-            let background = document.createElement("img"); //input element, text
-            background.className = "background";
-            if(i % 2 === 0){
-            background.src = 'https://i.imgur.com/SzVPn2I.png';
-            backgroundContainer.appendChild(background);
-            }
-            else{
-                background.src = 'https://images.pexels.com/photos/614810/pexels-photo-614810.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500';
-                backgroundContainer.appendChild(background);
-            }
-        }
         let leftClick = document.createElement('button');
         leftClick.id = 'left-click';
+        leftClick.textContent = '<'
         backgroundContainer.appendChild(leftClick);
+        let background = document.createElement("img"); //input element, text
+        background.className = "background";
+        background.src = backgroundArray[slideIndex]
+        backgroundContainer.appendChild(background);
         let rightClick = document.createElement('button');
         rightClick.id = 'right-click';
+        rightClick.textContent = ">"
         backgroundContainer.appendChild(rightClick);
         backgroundMid.appendChild(backgroundContainer);
         leftClick.addEventListener('click', function(){
-            plusDivs(-1)
+            slideIndex = incrementBackgroundIndex(false, slideIndex, backgroundArray);
+            background.src = backgroundArray[slideIndex]
+            buttonClickAudio.play();
         });
         rightClick.addEventListener('click', function(){
-            plusDivs(1)
+            slideIndex = incrementBackgroundIndex(true, slideIndex, backgroundArray);
+            background.src = backgroundArray[slideIndex]
+            buttonClickAudio.play();
         });
     }
+
     static renderBottom(backgroundBottom){
         let continueButton = document.createElement('button')
         continueButton.textContent = "Continue";
         backgroundBottom.appendChild(continueButton);
         continueButton.addEventListener('click', function(){
-            clearElements(bodyElement);
+            buttonClickAudio.play();
             FightPage.renderPage(bodyElement);
         })
     }
@@ -380,6 +386,8 @@ class FightPage{
         bodyElement.appendChild(fightTop);
         this.renderMid(fightMid);
         bodyElement.appendChild(fightMid);
+        fightMid.style.backgroundImage = `url(${backgroundArray[slideIndex]})`;
+        fightMid.style.backgroundSize = "100% auto"
         this.renderBottom(fightBottom);
         bodyElement.appendChild(fightBottom)
     }
@@ -399,6 +407,7 @@ class FightPage{
         chooseMid.appendChild(midSectionContainer);
         chooseMid.appendChild(fighterContainerRight);
 
+
         fetch(`http://localhost:3000/characters/${localStorage.getItem("fighterId")}`)
         .then(resp => resp.json())
         .then(json => {
@@ -415,7 +424,7 @@ class FightPage{
 
         localStorage.setItem("enemyId", randomEnemyId)
 
-        fetch(`http://localhost:3000/characters/${randomEnemyId}`) /* MAGIC NUMBER GROSSSSSSSSS */
+        fetch(`http://localhost:3000/characters/${randomEnemyId}`) 
         .then(resp => resp.json())
         .then(json => {
             fighterContainerRight = this.createFighterDOM(fighterContainerRight, true, json);
@@ -440,14 +449,15 @@ class FightPage{
         fightBottom.appendChild(forfeitButton);
 
         forfeitButton.addEventListener('click', function(){
-            clearElements(bodyElement);
+            buttonClickAudio.play();
             HomePage.renderPage(bodyElement);
         })
 
-        function combatRound(attackerId, defenderId, defenderDodge = false)
+        localStorage.setItem('defenderHealth', 100);
+        async function combatRound(attackerId, defenderId, defenderDodge = false)
         {
             console.log(attackerId, defenderId);
-            fetch('http://localhost:3000/characters',{
+            let response = await fetch('http://localhost:3000/characters',{
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -459,57 +469,44 @@ class FightPage{
                     defender_dodge: defenderDodge
                 })
             })
-            .then(resp => resp.json())
-            .then(json =>{
-                console.log(json)
-                if (json["defender_health"] < 0)
+            let defender = await response.json();
+            localStorage.setItem('defenderHealth', defender["defender_health"])
+            if (defender["defender_health"] <= 0)
+            {
+                if (defender["defender_id"] == localStorage.getItem("fighterId"))
                 {
-                    if (json["defender_id"] == localStorage.getItem("fighterId"))
-                    {
-                        document.querySelector(".fighter-1-health").textContent = 0;
-                        FightPage.playerLosePage();
-                    }
-                    else
-                    {
-                        document.querySelector(".fighter-2-health").textContent = 0;
-                        FightPage.playerWinPage();
-
-                    }
-                }  
-                else // if the fight is not over
-                {
-                    if (json["defender_id"] == localStorage.getItem("fighterId"))
-                    {
-                        document.querySelector(".fighter-1-health").textContent = json["defender_health"];
-                    }
-                    else
-                    {
-                        document.querySelector(".fighter-2-health").textContent = json["defender_health"];
-                    }
-
-                    // run animations stuff
-                    // wait a couple seconds
-                    // if this was the player's round
-                        // create and run combatRound for enemy
-                    // if this was the enemy's round
-                        // do nothing
+                    document.querySelector(".fighter-1-health").textContent = 0;
+                    FightPage.playerLosePage();
                 }
-
-            })
-            .catch()
+                else
+                {
+                    document.querySelector(".fighter-2-health").textContent = 0;
+                    FightPage.playerWinPage();
+                    // update wins
+                }
+            }  
+            else // if the fight is not over
+            {
+                if (defender["defender_id"] == localStorage.getItem("fighterId")) // if the player is the defender
+                {
+                    document.querySelector(".fighter-1-health").textContent = defender["defender_health"];
+                }
+                else // if the enemy is the defender
+                {
+                    document.querySelector(".fighter-2-health").textContent = defender["defender_health"]; 
+                }
+            }
         }
-        
-        attackButton.addEventListener('click', function(e){
-            combatRound(localStorage.getItem("fighterId"), localStorage.getItem("enemyId"));
-            setTimeout(combatRound(localStorage.getItem("enemyId"), localStorage.getItem("fighterId")), 10000);
-           
+        attackButton.addEventListener('click', async function(e){         
+            await combatRound(localStorage.getItem("fighterId"), localStorage.getItem("enemyId"))
+            if(localStorage.getItem('defenderHealth') > 0){
+                combatRound(localStorage.getItem("enemyId"), localStorage.getItem("fighterId"))
+            }   
         })
 
         dodgeButton.addEventListener('click', function(e){
             combatRound(localStorage.getItem("enemyId"), localStorage.getItem("fighterId"), true)
         })
-
-
     }
 
     static createFighterDOM(fighterContainer, onRightSide, fighterJSON = null)
@@ -524,9 +521,10 @@ class FightPage{
         fighterAnimation.src = fighterJSON["idle_gif"];
         fighterLabel.className = `fighter-${onRightSide + 1}-label`;
         fighterLabel.textContent = fighterJSON["name"]
-        fighterContainer.appendChild(fighterHealth);
-        fighterContainer.appendChild(fighterAnimation);
         fighterContainer.appendChild(fighterLabel);
+        fighterLabel.appendChild(fighterHealth);
+        fighterContainer.appendChild(fighterAnimation);
+        
     }
 
     static createMidSection(middleRegionContainer)
@@ -534,25 +532,40 @@ class FightPage{
         let middleRegionText = document.createElement('p');
         middleRegionContainer.className = "middle-region";
         middleRegionText.className = "middle-text";
-        middleRegionText.textContent = "##HERE WE GO##";
         middleRegionContainer.appendChild(middleRegionText);
     }
 
     static playerWinPage()
     {
-        // update mid section of web site
-        // update wins count of user
-        // update buttons
+        this.resultUpdate("You win you Winner");
+        localStorage.setItem('userWins', parseInt(localStorage.getItem('userWins')) + 1)
+        fetch(`http://localhost:3000/users/${localStorage.getItem('userId')}`, {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+                'Accepts': 'application/json'
+            },
+            body: JSON.stringify({
+                wins: localStorage.getItem('userWins')
+            })
+        })
+        .then(resp => resp.json())
+        .then(json => {
+            console.log(json)
+            console.log(localStorage.getItem('userWins'))
+        })
+        .catch()
     }
 
     static playerLosePage()
     {
-        // update mid section of web site
-        // update buttons
+        this.resultUpdate("You lose you Loser");
+    }
 
-        let middleText = document.querySelector('.middle-region');
-        middleText.textContent = "You Lose you loser";
-
+    static resultUpdate(message){
+        let middleText = document.querySelector('.middle-text');
+        middleText.textContent = message;
+        console.log(middleText.textContent)
         let buttonContainer = document.querySelector('.fight-bottom');
         clearElements(buttonContainer);
         let playAgain = document.createElement('button');
@@ -565,35 +578,60 @@ class FightPage{
         buttonContainer.appendChild(exitGame);
         playAgain.addEventListener('click', function(e){
             CharacterSelection.renderPage(bodyElement);
+            buttonClickAudio.play()
         })
         exitGame.addEventListener('click', function(e){
             HomePage.renderPage(bodyElement);
+            buttonClickAudio.play()
         })
-
     }
 }
 /*-------------------------------------------------------------------------*/
 
 /*-------------------- This section is for creating the slides for the background -----*/
-let slideIndex = 1;
-function plusDivs(n) {
-    showDivs(slideIndex += n);
-}
-function showDivs(n) {
-    let i;
-    let x = document.getElementsByClassName("background");
-
-    if (n > x.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = x.length}
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";  
+let slideIndex = 0;
+let backgroundArray = ["https://i.imgur.com/Lg2omfl.gif","https://i.imgur.com/8FoYKgc.gif", "https://i.imgur.com/avuyoaD.gif", "https://i.imgur.com/wp3leDc.gif", "https://i.imgur.com/K7fu07T.gif", "https://i.imgur.com/YWCqjwV.gif", "https://i.imgur.com/Sx7PROj.gif"]
+let buttonClickAudio = new Audio('https://freesound.org/data/previews/322/322228_5048136-lq.mp3');
+buttonClickAudio.type = 'audio/mp3'
+function incrementBackgroundIndex(increment, currentIndex, sourceArray)
+{
+    if (increment === true)
+    {
+        if (currentIndex + 1 >= sourceArray.length)
+        {
+            return 0
+        }
+        else
+        {
+            return ++currentIndex        
+        }
     }
-    x[slideIndex-1].style.display = "block";  
+    else
+    {
+        if (currentIndex - 1 < 0)
+        {
+            return sourceArray.length-1
+        }
+        else
+        {
+            return --currentIndex        
+        }
+    }
+
 }
 /*-------------------------------------------------------------------------------------*/
+function playBackground(){
+    if (!document.getElementById("background-music")){
+        let audioTest = document.createElement('audio');
+        audioTest.id = "background-music"
+        audioTest.src = 'https://mod3-project.s3-us-west-2.amazonaws.com/Premium+retro+swing+music+for+Cooking+Shows+and+Videos+-+Food+Show+Kochshow+Music.mp3'
+        document.head.appendChild(audioTest)
+        audioTest.loop = true;
+        audioTest.play()
+    }
+}
 
 document.addEventListener("DOMContentLoaded", function(e){
-
 
     bodyElement = document.querySelector('body');
     HomePage.renderPage(bodyElement);
